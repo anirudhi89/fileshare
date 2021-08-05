ab6a1c8d3a6750b69ed088b3e352202a0195d79a
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-08-04 20:26:16 -0700

    general idea/frontend working

---
+++ public/receiver.js
@@ -0,0 +1,23 @@
+(function(){
+
+	const socket = io();
+	let sender_uid;
+
+	function generateID(){
+		return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
+	}
+
+	document.querySelector("#receiver-start-con-btn").addEventListener("click",function(){
+		sender_uid = document.querySelector("#join-id").value;
+		if(sender_uid.length == 0){
+			return;
+		}
+		let joinID = generateID();
+		socket.emit("receiver-join", {
+			sender_uid:sender_uid,
+			uid:joinID
+		});
+		document.querySelector(".join-screen").classList.remove("active");
+		document.querySelector(".fs-screen").classList.add("active");
+	});
+})()
